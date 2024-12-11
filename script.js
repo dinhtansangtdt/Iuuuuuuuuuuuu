18a3682c6eb887616d70a8e38d257cb720bb5ba4
Đinh Tấn Sang <76915853+dinhtansangtdt@users.noreply.github.com>
2024-12-11 15:51:46 +0700

    yesss

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const elts = {
     text2: document.getElementById("text2"),
 };
 
-const texts = ["Nhấn", "vào", "Trái", "Tim", "❤️"];
+const texts = ["Nhấn", "vào", "Trái", "Tim", "Nho", "Nhật Quy","❤️"];
 
 const morphTime = 1;
 const cooldownTime = 0.25;
